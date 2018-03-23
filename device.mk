ca6fa29889555eabb9237765f52c6efb251e1ae4
Alberto97 <albertop2197@gmail.com>
2018-03-23 21:17:31 +0100

    Move out of multihal

--- device.mk
+++ device.mk
@@ -276,9 +276,6 @@ PRODUCT_PACKAGES += \
     sensorhub.msm8953 \
     sensors.msm8953
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
-
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
